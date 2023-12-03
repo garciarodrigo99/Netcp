e2a23646e7a0e56d6dfe355c87afbf1a10b6d150
Rodrigo <alu0101154473@ull.edu.es>
2023-12-03 21:37:51 +0000

    Añadido getenv

--- netcp.cpp
+++ netcp.cpp
@@ -1,5 +1,12 @@
 // g++-13 -o netcp -std=c++23 netcp.cpp
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <cstdint>
+#include <system_error>
+#include <vector>
+#include <optional>
+//#include <expected>
 
 /*
  * ERROR 1: Falta nombre de archivo en los parametros de ejecución
@@ -9,11 +16,39 @@ void help(char* program_name){
 	printf("Modo de empleo: %s [-h] ORIGEN\n",program_name);
 }
 
+/*
+* Dada una cadena std::string, obtener la variable de entorno en un objeto 
+* std::string
+*/
+std::string getenv(const std::string& name)
+{
+	char* value = getenv(name.c_str());
+	if (value) {
+		return std::string(value);
+	}
+	else {
+		return std::string();
+	}
+}
+
+// std::error_code read_file(int fd, const std::vector<uint8_t>& buffer);
+
+// std::optional<sockaddr_in> make_ip_address(
+// 							const std::optional<std::string> ip_address, 
+// 							uint16_t port=0);
+
+// using make_socket_result = std::expected<int, std::error_code>;
+
+// make_socket_result make_socket(std::optional<sockaddr_in> address = std::nullopt);
+// std::error_code send_to(int fd, 
+// 						const std::vector<uint8_t>& message,
+// 						const sockaddr_in& address);
+
 int main(int args, char* argv[]){
 
 	// Comprobar argumento 1
 	if (args == 1){
-		printf("%s: falta un archivo como argumento\n",argv[0]);
+		fprintf(stderr,"%s: falta un archivo como argumento\n",argv[0]);
 		help(argv[0]);
 		return 1;
 	}

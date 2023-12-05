ad7d4f77fca625df27acf889278f912fbc3dcd0e
Rodrigo <alu0101154473@ull.edu.es>
2023-12-05 23:50:12 +0000

    open and read ok

--- netcp-code/file_functions.hpp
+++ netcp-code/file_functions.hpp
@@ -7,6 +7,17 @@
 #include <fcntl.h>
 
 [[nodiscard]]
+std::expected<int, std::error_code> open_file(const std::string& path, 
+                                                int flags, mode_t mode) {
+    int fd = open(path.c_str(), flags, mode);
+    if (fd == -1) {
+        std::error_code error(errno, std::system_category());
+        return std::unexpected(error);
+    }
+    return fd;
+}
+
+[[nodiscard]]
 std::error_code read_file(int fd, std::vector<uint8_t>& buffer) {
     ssize_t bytes_read = read(fd, buffer.data(), buffer.size());
     if (bytes_read < 0) {
@@ -16,13 +27,4 @@ std::error_code read_file(int fd, std::vector<uint8_t>& buffer) {
     return std::error_code(0, std::system_category());
 }
 
-std::expected<int, std::error_code> open_file(const std::string& path, 
-                                                int flags, mode_t mode) {
-    int fd = open(path.c_str(), flags, mode);
-    if (fd == -1)
-    {
-    std::error_code error(errno, std::system_category());
-    return std::unexpected(error);
-    }
-    return fd;
-}
+std::error_code write_file(int fd, const std::vector<uint8_t>& buffer);
--- netcp-code/netcp.cpp
+++ netcp-code/netcp.cpp
@@ -1,4 +1,6 @@
 // g++-13 -o netcp -std=c++20 -g netcp.cpp menu.cpp
+// Generación aleatoria de solo numeros y caracteres:
+// LC_CTYPE=C tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 1024 >testfile
 
 #include "menu.hpp"
 #include "socket_functions.hpp"
@@ -47,13 +49,36 @@ int main(int args, char* argv[]){
 	if (options.value().output_filename.empty()){
 		netcpErrorExit(Netcp_errors::FILE_MISSING_ERROR);
 	}
+
+	std::string file_path = "testfile.txt";  // Reemplaza con la ruta a tu archivo
+    int flags = O_RDONLY;
+    mode_t mode = 0;  // Modo no relevante para la apertura de lectura
+
+    auto result = open_file(file_path, flags, mode);
+    if (!result) {
+        std::cerr << "Error al abrir el archivo: " << result.error().message() << std::endl;
+        return result.error().value();
+    }
+
+    int fd = result.value();
+    std::vector<uint8_t> buffer(1024);  // Tamaño del buffer, puedes ajustarlo según tus necesidades
+
+    auto read_result = read_file(fd, buffer);
+	std::cout << "read_result: " << read_result << std::endl;
+    if (!read_result) {
+        std::cout.write(reinterpret_cast<const char*>(buffer.data()), buffer.size());
+        std::cout << std::endl;
+    } else {
+        std::cerr << "Error al leer el archivo: " << read_result.message() << std::endl;
+        return read_result.value();
+    }
 	
-	auto address = make_ip_address(std::nullopt);
-	auto socket = make_socket(address.value());
-	if (socket) {
-		auto sock_fd = socket.value();
-		std::cout << sock_fd << std::endl;
-	}
+	// auto address = make_ip_address(std::nullopt);
+	// auto socket = make_socket(address.value());
+	// if (socket) {
+	// 	auto sock_fd = socket.value();
+	// 	std::cout << sock_fd << std::endl;
+	// }
 
   return EXIT_SUCCESS;
 }

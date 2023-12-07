60510024c9a66b782fc6268a54ed6992ba8b8901
Rodrigo <alu0101154473@ull.edu.es>
2023-12-07 13:47:38 +0000

    Reestructuración en rama main

--- netcp_errors.hpp
+++
@@ -1,7 +0,0 @@
-// Definición de una struct para constantes
-#pragma once
-struct Netcp_errors {
-    static const int FILE_MISSING_ERROR = 1;
-    static const int SOCK_CREATION_ERROR = 3;
-    static const int ADDRESS_ASSIGNMENT_ERROR = 4;
-};
